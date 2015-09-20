fb12450a47eed14e3c01ccab70bf5ea1f61cd109
Jun - Home <junc76@gmail.com>
2015-09-20 21:21:59 +0800

    Use pandoc to convert markdown to pdf

---
+++ Stock/convert.sh
@@ -0,0 +1,9 @@
+
+pandoc calculate_profit.md -o calculate_profit.pdf --smart --template=pm-template --latex-engine=xelatex -V mainfont='STHeiti'
+
+# brew cask install pandoc
+# brew cask install basictex
+
+# sudo tlmgr --self --all update
+# sudo tlmgr install titling
+# sudo tlmgr install lastpage
